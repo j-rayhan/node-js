8c6e553c946b4a1130b0eec3aac7b72ef76c95ce
rafser01 <r.afser01@gmail.com>
2019-06-15 17:32:37 +0600

    [IMPORT] npm module and use it

--- 4-lesson/app.js
+++ 4-lesson/app.js
@@ -1,12 +1,5 @@
-const fs = require('fs');
+const validator = require('validator');
 
-// fs.writeFileSync('notes.txt', 'Hi I am Rayhan.');
-
-fs.appendFileSync('notes.txt', "\nI live in bangladesh")
-
-//  Challenge: Append a message to notes.txt
-// =================== 000 =================
-//  1. use appendFileSync to append to the File
-//  2. Run the script 
-//  3. Check your work by opening the file and viewing the append text 
-
+const isEmail = validator.isEmail('add@gmail.com');
+const isNumber = validator.isNumeric(' 4');
+console.log('======>', isNumber)

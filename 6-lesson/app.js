d94b35c955b572fbf67d8595dac952fa4feede81
rafser01 <r.afser01@gmail.com>
2019-06-15 18:40:34 +0600

    [END-OF-SECTION=>3]

--- 6-lesson/app.js
+++ 6-lesson/app.js
@@ -1,2 +1,18 @@
+const chalk = require('chalk');
 
+const log = console.log;
+
+const greenMsg = chalk.green('Success!');
+const msg = chalk.blue.bgBlue.bold('Hello world 2!');
+
+log(greenMsg);
+log(msg);
+//  Challenge: use the chalk library in your project
+// =================== 000 =================
+//  1. Install version 2.4.1 of chalk
+//  2. Load chalk into app.js
+//  3. Use it to print the string "Success!" to the console in green
+//  4. Test your work
+
+// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
 

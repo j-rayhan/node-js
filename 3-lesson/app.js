5ad949497542c747f78074fb5bc4783444fd6a5f
rafser01 <r.afser01@gmail.com>
2019-06-15 17:10:43 +0600

    [TO-DO]

--- 3-lesson/app.js
+++ 3-lesson/app.js
@@ -4,3 +4,12 @@ const firstName = utils.name;
 const sum = utils.add(8,-2);
 
 console.log(sum);
+
+// ========================== 000 =====================
+
+// Challenge: Define and use a function in a new file
+
+// 1. Create a new file called notes.js
+// 2. Create getNotes function that returns "your notes ....."
+// 3. Export getNotes function
+// 4. From app.js, load in and call the function printing message to console

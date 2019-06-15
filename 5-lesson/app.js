f520d0523342c64c356a74639c066aa54e1cb110
rafser01 <r.afser01@gmail.com>
2019-06-15 18:07:35 +0600

    [TO-DO] install and import and use

---
+++ 5-lesson/app.js
@@ -0,0 +1,10 @@
+
+//  Challenge: use the chalk library in your project
+// =================== 000 =================
+//  1. Install version 2.4.1 of chalk
+//  2. Load chalk into app.js
+//  3. Use it to print the string "Success!" to the console in green
+//  4. Test your work
+
+// Bonus: Use docs to mess around with other styles. Make text bold and inversed.
+

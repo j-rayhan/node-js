ad7014b811f4bc586e0a20c5ab4c8e2e5cefaf57
rafser01 <r.afser01@gmail.com>
2019-06-15 17:22:53 +0600

    [ADD] Create package.json

---
+++ 4-lesson/app.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+// fs.writeFileSync('notes.txt', 'Hi I am Rayhan.');
+
+fs.appendFileSync('notes.txt', "\nI live in bangladesh")
+
+//  Challenge: Append a message to notes.txt
+// =================== 000 =================
+//  1. use appendFileSync to append to the File
+//  2. Run the script 
+//  3. Check your work by opening the file and viewing the append text 
+

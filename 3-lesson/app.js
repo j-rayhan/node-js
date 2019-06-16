c231939c120f477d334ace3505bb5792d5a0ecda
rafser01 <r.afser01@gmail.com>
2019-06-16 16:26:36 +0600

    [DIFF] More Yargs and Process

--- 3-lesson/app.js
+++ 3-lesson/app.js
@@ -1,4 +1,12 @@
 const yargs = require('yargs');
 const command = process.argv;
+
+/**
+ * Run this in Terminal to show more diff
+ * node app.js --title="Hi I am Rayhan"
+ * OR
+ * node app.js Johir --title="Hi I am Rayhan"
+ * 
+ */
 console.log('PRINT IN %s=====>','Process', command);
 console.log('PRINT IN %s=====>','Yargs',yargs.argv);

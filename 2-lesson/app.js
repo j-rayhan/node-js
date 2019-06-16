b8d28eabeb57a554ea72e0086a45700671dd6ca5
rafser01 <r.afser01@gmail.com>
2019-06-16 16:14:00 +0600

    [GET] spacifick argment from command line

--- 2-lesson/app.js
+++ 2-lesson/app.js
@@ -1,11 +1,6 @@
 
-console.log('PRINT IN %s=====>','App', process.argv);
-/**
- * node app.js Rayhan 
- * 
- * OUT-PUT
- * [ '/home/sazzad/.nvm/versions/node/v8.12.0/bin/node',
-  '/home/sazzad/all-test/node-js/tutorial-1/2-lesson/app.js',
-  'Rayhan' ]
- * 
- */
+const command = process.argv[2];
+
+if (command === 'add') {
+    console.log('PRINT IN %s=====>','App');
+}

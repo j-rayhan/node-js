89dd73326c4e461512157a0adc7465f9dc9bd5ca
rafser01 <r.afser01@gmail.com>
2019-06-16 16:43:07 +0600

    [TO-DO]

--- 3-lesson/app.js
+++ 3-lesson/app.js
@@ -17,9 +17,11 @@ yargs.command({
     }
 })
 /**
- * Run 
- * node app.js add
- * node app.js remove
+ *  Challenge: Add two new commands
+ * 
+ * 1. Setup command to support "list" command (print placeholder message for now)
+ * 2. Setup command to support "read" command (print placeholder message for now)
+ * 3. Test your work by running both commands and ensure correct output
  * 
  */
 console.log(yargs.argv)

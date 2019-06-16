53dfe7daacbf1332df08b3ddd07239f85de53707
rafser01 <r.afser01@gmail.com>
2019-06-16 16:08:23 +0600

    [ADD] argment in command line

---
+++ 2-lesson/app.js
@@ -0,0 +1,11 @@
+
+console.log('PRINT IN %s=====>','App', process.argv);
+/**
+ * node app.js Rayhan 
+ * 
+ * OUT-PUT
+ * [ '/home/sazzad/.nvm/versions/node/v8.12.0/bin/node',
+  '/home/sazzad/all-test/node-js/tutorial-1/2-lesson/app.js',
+  'Rayhan' ]
+ * 
+ */

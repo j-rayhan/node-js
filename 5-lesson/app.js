2afe009e21adf1be14c7407ab31898e919f1ac39
rafser01 <r.afser01@gmail.com>
2019-06-17 17:06:25 +0600

    [TO-DO]

--- 5-lesson/app.js
+++ 5-lesson/app.js
@@ -5,4 +5,11 @@ const dataJson = bufferData.toString();
 const data = JSON.parse(dataJson);
 console.log(data.title);
 
-
+/**
+ *  Challenge: Work with JSON and the file system
+ * 
+ * 1. Load and parse the JSON data
+ * 2. Change the name and age property using your info
+ * 3. Stringfy the changed object and overwrite the original data
+ * 4. Test your work by viewing data in the JSON file
+ */

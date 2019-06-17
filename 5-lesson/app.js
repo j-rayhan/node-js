605ff859915b34ea85c7afe40cb7ac83c0590507
rafser01 <r.afser01@gmail.com>
2019-06-17 17:27:01 +0600

    [SOLVE--->TO-DO]

--- 5-lesson/app.js
+++ 5-lesson/app.js
@@ -1,15 +1,25 @@
 const fs = require('fs');
 
-const bufferData = fs.readFileSync('book.json');
-const dataJson = bufferData.toString();
-const data = JSON.parse(dataJson);
-console.log(data.title);
+// const bufferData = fs.readFileSync('book.json');
+// const dataJson = bufferData.toString();
+// const data = JSON.parse(dataJson);
+// console.log(data.title);
 
 /**
  *  Challenge: Work with JSON and the file system
  * 
  * 1. Load and parse the JSON data
  * 2. Change the name and age property using your info
- * 3. Stringfy the changed object and overwrite the original data
+ * 3. Stringify the changed object and overwrite the original data
  * 4. Test your work by viewing data in the JSON file
  */
+const bufferData = fs.readFileSync('book.json');
+const dataJson = bufferData.toString();
+const data = JSON.parse(dataJson);
+
+data.title = "New title"
+data.author = "Johir Ray"
+
+const JSONdata = JSON.stringify(data);
+
+fs.writeFileSync('book.json', JSONdata);

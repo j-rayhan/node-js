43d4070d82ac1e322f8ee75cf7ff29225e03ea67
rafser01 <r.afser01@gmail.com>
2019-06-17 16:38:26 +0600

    [CREATE] json file

--- 5-lesson/app.js
+++ 5-lesson/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const book = {
     title: "Good Life",
     author: "Rayhan Dev"
@@ -5,11 +6,13 @@ const book = {
 
 const bookJson = JSON.stringify(book);
 
+fs.writeFileSync('book.json', bookJson)
+
 // console.log(bookJson);
 
 // console.log(bookJson.title);
 
-const parseBook = JSON.parse(bookJson);
+// const parseBook = JSON.parse(bookJson);
 
-console.log(parseBook.author);
+// console.log(parseBook.author);
 
